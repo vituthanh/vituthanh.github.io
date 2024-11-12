ae3a219271b5ad15c0cf4cd17d851bf55d2d1c72
tv_thanh <tv_thanh@brycen.com.vn>
2024-11-12 14:22:17 +0700

    .

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,26 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      screens: {
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        "2xl": "1400px",
+      },
+      maxWidth: {
+        sm: "540px",
+        md: "720px",
+        lg: "960px",
+        xl: "1140px",
+        "2xl": "1320px",
+      },
+    },
 	},
 	plugins: [
     require('@tailwindcss/typography')
