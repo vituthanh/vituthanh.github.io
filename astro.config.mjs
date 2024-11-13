baef5778337d43649eb800b1ffe9c02e1082bc29
tv_thanh <tv_thanh@brycen.com.vn>
2024-11-13 10:25:35 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from '@astrojs/preact';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vituthanh.github.io',
-  integrations: [preact(), tailwind()]
+  integrations: [tailwind()]
 });
