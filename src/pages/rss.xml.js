c1a8879792eb481a09f09ae9e469c0e7f522fd65
tv_thanh <tv_thanh@brycen.com.vn>
2024-11-13 10:45:59 +0700

    .

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,11 +2,17 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
+  const posts = await getCollection('posts');
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('../content/posts/**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
